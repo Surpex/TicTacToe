08051c65b392c4a16be5f2414cd9bb08d54c6efb
Takoda Kerr <kerrtak@gmail.com>
2022-05-18 21:56:54 -0400

    Changed the way that buttons are created, modified, and added to the frame. Added a parallel array to make win-checking easier. Implemented a win-checking solution that doesn't seem to be working. Broke other methods out to a driver method for a cleaner main();

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -15,164 +15,58 @@ public class TicTacToeGame extends JPanel{
     private static ImageIcon xMark;
     private static ImageIcon oMark;
 
-    //holds all the buttons. I've done this primarily to
-    //make it easier to check if the game has been won,
-    //since I can just reference different indexes of the
-    //array instead of trying to pull the button out of the
-    //JFrame (if you can even do that)
+    //holds all the buttons. I
     private static TicButton[] buttonArray = new TicButton[9];
+
+    //parallel array that holds a representation of the board.
+    //X represents the cross, O represents the circle. This
+    //will allow for easier checking of win states. boardArray
+    //is initialized with the character 'u'.
+    private static char[] boardArray = new char[9];
 
 
     public static void main(String[] args){
-        //assigns values to the xMark and oMark ImageIcons.
-        manipulateIcons();
-
-        //Make new JFrame for main window, set title
-        JFrame gameWindow = new JFrame();
-        gameWindow.setTitle("Tic Tac Toe!");
-
-        //exits using the System exit method. Can also have it
-        //hide on close instead of close.
-        gameWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-        //changes the icon
-        ImageIcon windowIcon = new ImageIcon("resources/windowIcon.gif");
-        gameWindow.setIconImage(windowIcon.getImage());
-
-        //sets window size and if the window can be resized
-        gameWindow.setSize(500, 500);
-        gameWindow.setResizable(false);
-
-        //removes the 1-2 pixel gap on the right and left of the
-        //play squares. Unsure what causes them, but this fixes them.
-        gameWindow.getRootPane().setBorder(new EmptyBorder(0, -1, 0, -1));
-
-        //-------Testing gridlayout-------
-        gameWindow.setLayout(new GridLayout(3,3, 14, 14));
-
-        //set BG color, changes the color between buttons
-        gameWindow.getContentPane().setBackground(Color.BLACK);
-
-        addButtons(gameWindow);
-
-        //displays the window
-        gameWindow.setVisible(true);
-
-        //centers on the screen because no other component is
-        //specified. Could also use setLocationRelativeTo(compName)
-        //which would "..the windows is located in such a way that
-        //the center of the window coincides with the center of the
-        //[parameter] component". Neat!
-//        gameWindow.setLocationRelativeTo(null);
-
+        driver();
     }
 
+    //TODO: Make a parallel array that will hold a single character
+    // representation of the play mark (X for the cross, O for the circle)
 
-    //TODO: add buttons into global array buttonArray[],
-    // add game logic using that array
+    //creates buttons, adds them to global array buttonArray[],
+    // attaches event listeners to them, adds them to gWindow
     private static void addButtons(JFrame gWindow){
-
-
-        //private static TicButton[] buttonArray = new TicButton[9];
-
-        TicButton button1 = new TicButton();
-        TicButton button2 = new TicButton();
-        TicButton button3 = new TicButton();
-        TicButton button4 = new TicButton();
-        TicButton button5 = new TicButton();
-        TicButton button6 = new TicButton();
-        TicButton button7 = new TicButton();
-        TicButton button8 = new TicButton();
-        TicButton button9 = new TicButton();
-
-        button1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                placeMark(button1);
-            }
-        });
-        button2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                placeMark(button2);
-            }
-        });
-        button3.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                placeMark(button3);
-            }
-        });
-        button4.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                placeMark(button4);
-            }
-        });
-        button5.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                placeMark(button5);
-            }
-        });
-        button6.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                placeMark(button6);
-            }
-        });
-        button7.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                placeMark(button7);
-            }
-        });
-        button8.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                placeMark(button8);
-            }
-        });
-        button9.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                placeMark(button9);
-            }
-        });
-
-//        //add buttons to array
-//        buttonArray[0] = button1;
-
-        //add buttons to JFrame
-        gWindow.add(button1);
-        gWindow.add(button2);
-        gWindow.add(button3);
-        gWindow.add(button4);
-        gWindow.add(button5);
-        gWindow.add(button6);
-        gWindow.add(button7);
-        gWindow.add(button8);
-        gWindow.add(button9);
+        for (int i = 0; i < 9; i++){
+            buttonArray[i] = new TicButton();
+            //for debugging
+//            buttonArray[i] = new TicButton(Integer.toString(i));
+            int finalI = i;
+            buttonArray[i].addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    placeMark(buttonArray[finalI]);
+                }
+            });
+            gWindow.add(buttonArray[i]);
+        }
     }
-
-//    TESTING CODE, NO LONGER NEEDED
-//    private static void swapBGTest   (JFrame gWindow){
-//        if (isXTurn){
-//            gWindow.getContentPane().setBackground(Color.red);
-//        } else {
-//            gWindow.getContentPane().setBackground(Color.blue);
-//        }
-//        isXTurn = !isXTurn;
-//    }
 
     //places the mark depending on if it is currently X's turn
     //or O's turn, then it flips the global bool isXTurn to
     //keep track of whose turn it is
+
+    //TODO: This isn't working correctly. Inputting a
+    // winning combo doesn't output to console, which is
+    // the expected output.
     private static void placeMark(TicButton button){
         if (isXTurn) {
             button.setIcon(xMark);
         } else {
             button.setIcon(oMark);
+        }
+
+        //check if the game has been won
+        if(checkWin()){
+            System.out.println("Game has been won");
         }
         isXTurn = !isXTurn;
     }
@@ -191,5 +85,97 @@ public class TicTacToeGame extends JPanel{
         img = icon.getImage();
         newImg = img.getScaledInstance(130, 130, Image.SCALE_SMOOTH);
         oMark = new ImageIcon(newImg);
+    }
+
+    private static void initArray(){
+        for (int i = 0; i < 9; i++){
+            boardArray[i] = 'u';
+        }
+    }
+
+    private static void driver(){
+        //assigns values to the xMark and oMark ImageIcons.
+        manipulateIcons();
+
+        //initializes parallel array
+        initArray();
+
+        //Make new JFrame for main window, set title
+        JFrame gameWindow = new JFrame();
+        gameWindow.setTitle("Tic Tac Toe!");
+
+        //exits using the System exit method. Can also have it
+        //hide on close instead of close.
+        gameWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        //changes the icon
+        ImageIcon windowIcon = new ImageIcon("resources/windowIcon.gif");
+        gameWindow.setIconImage(windowIcon.getImage());
+
+        //sets window size and if the window can be resized
+        gameWindow.setSize(500, 500);
+        gameWindow.setResizable(false);
+
+        //removes the 1-2 pixel gap on the right and left of the
+        //play squares. Unsure what causes them, but this fixes them
+        gameWindow.getRootPane().setBorder(new EmptyBorder(0, -1, 0, -1));
+
+        //-------Testing gridlayout-------
+        gameWindow.setLayout(new GridLayout(3,3, 14, 14));
+
+        //set BG color, changes the color between buttons
+        gameWindow.getContentPane().setBackground(Color.BLACK);
+
+        addButtons(gameWindow);
+
+        //displays the window
+        gameWindow.setVisible(true);
+    }
+
+    private static boolean  checkWin(){
+        boolean hasWon = false;
+        if (checkVerticals() || checkHorizontals() || checkDiagonals()){
+            hasWon = true;
+        }
+        return hasWon;
+    }
+
+    //How the array maps to the board:
+    //[0,1,2]
+    //[3,4,5]
+    //[6,7,8]
+    private static boolean checkVerticals(){
+
+        for (int i = 0; i < 3; i++){
+            if (boardArray[i] != 'u'){
+                if ((boardArray[i] == boardArray[i + 3]) && (boardArray[i + 3] == boardArray[i + 6])){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkHorizontals(){
+        for (int i = 0; i < 7; i += 3){
+            if (boardArray[i] != 'u'){
+                if((boardArray[i] == boardArray[i + 1]) && (boardArray[i + 1] == boardArray[i + 2])){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkDiagonals(){
+        if (boardArray[0] != 'u'){
+            if ((boardArray[0] == boardArray[4]) && (boardArray[4] == boardArray[8])){
+                return true;
+            }
+            else if ((boardArray[2] == boardArray[4]) && (boardArray[4] == boardArray[8])){
+                return true;
+            }
+        }
+        return false;
     }
 }
